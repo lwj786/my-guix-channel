e4875881c5b4aa0c00277326aa417018d2d6fb7b
Li Wujun <li.wujun@wujun.li>
2025-08-06 00:15:33 +0800

    add package:   - wayback 0.2

---
+++ gong/packages/wm.scm
@@ -0,0 +1,39 @@
+(define-module (gong packages wm)
+  #:use-module (guix packages)
+  #:use-module (guix git-download)
+  #:use-module (guix build-system meson)
+  #:use-module ((guix licenses) #:prefix license:)
+  #:use-module (gnu packages freedesktop)
+  #:use-module (gnu packages xdisorg)
+  #:use-module (gnu packages wm)
+  #:use-module (gnu packages xorg)
+  #:use-module (gnu packages pkg-config)
+  #:use-module (gnu packages man))
+
+
+(define-public wayback
+  (package
+    (name "wayback")
+    (version "0.2")
+    (source
+     (origin
+       (method git-fetch)
+       (uri (git-reference
+              (url "https://gitlab.freedesktop.org/wayback/wayback")
+              (commit version)))
+       (file-name (git-file-name name version))
+       (sha256
+        (base32 "1z9p2naw1ff5g8k990nilzplldr4dqjqlz945nfqwf5vvgbdd5zj"))))
+    (build-system meson-build-system)
+    (inputs (list wayland
+                  wayland-protocols
+                  libxkbcommon
+                  wlroots
+                  xorg-server-xwayland))
+    (native-inputs (list pkg-config
+                         scdoc))
+    (home-page "https://wayback.freedesktop.org")
+    (synopsis "X11 compatibility layer leveraging wlroots and Xwayland")
+    (description "Wayback is a X11 compatibility layer which allows for running full X11
+desktop environments using Wayland components.")
+    (license license:expat)))
